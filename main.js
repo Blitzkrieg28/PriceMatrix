f483836bba00be101295b0ef170afa13d25e2daa
Blitzkreig28 <tanatwork2809@gmail.com>
2025-12-27 20:13:54 +0000

    Extraction and Transformation for amazon website done

---
+++ main.js
@@ -0,0 +1,21 @@
+const {scrapeAmazonProd}= require('./src/scrapers/amazon');
+
+async function startEng(){
+
+    console.log("starting v1 engine...");
+    const tarUrl = 'https://www.amazon.in/Oppo-Storage-Additional-Exchange-Offers/dp/B0G3XGKMJX/?_encoding=UTF8&pd_rd_w=gabP0&content-id=amzn1.sym.47226dd1-3657-494d-8578-f5621c2124b3&pf_rd_p=47226dd1-3657-494d-8578-f5621c2124b3&pf_rd_r=6QWW378HWWR1G2YMEK4K&pd_rd_wg=ehjtl&pd_rd_r=b391185a-dbbd-49fc-975e-955e195bedfd&ref_=pd_hp_d_atf_dealz_cs&th=1';
+
+    console.log(`requesting data for: ${tarUrl.substring(0,40)}...`);
+
+    const prodData=await scrapeAmazonProd(tarUrl);
+
+    if(prodData){
+        console.log("scrapping successfull!!");
+        console.log(prodData);
+    }
+    else{
+        console.log("something went wrong!!");
+    }
+}
+
+startEng();
---
+++ src/scrapers/amazon.js
@@ -0,0 +1,71 @@
+const puppeteer= require('puppeteer-extra');
+const StealthPlugin=  require('puppeteer-extra-plugin-stealth');
+
+puppeteer.use(StealthPlugin());
+
+async function scrapeAmazonProd(url){
+    console.log("initiating...");
+    let browser= null;
+     
+    try{
+     browser= await puppeteer.launch({
+        headless: "new",
+        // slowMo: 100,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page= await browser.newPage();
+
+    await page.setViewport({ width: 1920, height: 1080 });
+    //const amazonUrl = 'https://www.amazon.in/dp/B0FZT1LXPZ/?_encoding=UTF8&ref_=cct_cg_Budget_2a1&pf_rd_p=e375775e-f345-480d-adaf-5496c142308d&pf_rd_r=TSWS6SRPESSYDB9ZBC1V&th=1';
+
+    console.log("navigating to the page..");
+    await page.goto(url, { waitUntil: 'domcontentloaded',timeout: 30000 });
+
+    const pageTitle= await page.title();
+    if (pageTitle.includes("Amazon")) {
+        console.log(" Soft block detected! Attempting to bypass...");
+        
+        try {
+            const button = await page.waitForSelector('button, input[type="submit"]', { timeout: 3000 });
+            
+            if (button) {
+                console.log(" Clicking the 'Continue' button...");
+                await button.click();
+                
+                await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 10000 });
+                console.log(" Navigation complete. Back on track!");
+            }
+        } catch (err) {
+            console.log(" Could not find a button to click. We might be truly stuck.");
+        }
+    }
+
+        console.log("waiting for the css selector..");
+        await page.waitForSelector('.a-price-whole', { timeout: 20000 });
+        
+        console.log("extracting title and price!!");
+        const data= await page.evaluate(()=>{
+            const titleEle= document.querySelector('#productTitle');
+            const priceEle= document.querySelector('.a-price-whole');
+
+      const title = titleEle ? titleEle.innerText.trim() : "Unknown Title";
+      const rawPrice = priceEle ? priceEle.innerText : "0";
+
+      const cleanPrice= rawPrice.replace(/[^0-9]/g, '');
+
+      return {
+        title,
+        price: parseInt(cleanPrice) || 0,
+        scrapedAt: new Date().toISOString()
+      };
+        });
+    await browser.close();
+    return data;
+} catch(err){
+    console.error("module error: ",err.message);
+    if(browser) await browser.close();
+    return null;
+}
+}
+
+module.exports= {scrapeAmazonProd};
